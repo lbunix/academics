5cdc052b127d02a5dcd5302592dd7a3e7802ca3d
lbunix <loc@lbunix.com>
2026-02-03 18:00:43 -0800

    Update net-check.sh

--- CIS217/net-check.sh
+++ CIS217/net-check.sh
@@ -9,12 +9,12 @@ echo -e "This is your current IP: \n$ipaddr\n"
 echo -en "Let's check the DNS of a website. Please input the website URL: "
 read site
 
-echo -e "The DNS information of the site you had input is: "
+echo -e "\n\nThe DNS information of the site you had input is: "
 host -t ns "$site"
 sleep 1
 
-echo -e "\nLet's run a trace to the site you had chosen.\n"
-echo -e "Checking...\n\n"
+echo -e "\n\nLet's run a trace to the site you had chosen.\n"
+echo -e "Checking...\n"
 sleep 2
 
 traceroute "${site}"
