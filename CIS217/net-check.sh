92e856a98fe53f513bd7e5020131da45656fec6e
lbunix <loc@lbunix.com>
2026-02-03 17:56:24 -0800

    Update net-check.sh

--- CIS217/net-check.sh
+++ CIS217/net-check.sh
@@ -5,7 +5,7 @@ ipaddr=$(ip a | grep inet | grep -v -e "127" -e "inet6")
 echo -e "\n### Initiating Net Check Script ###\n\n"
 sleep 2
 
-echo -e "This is your current IP: $ipaddr\n"
+echo -e "This is your current IP: \n$ipaddr\n"
 echo -en "Let's check the DNS of a website. Please input the website URL: "
 read site
 
