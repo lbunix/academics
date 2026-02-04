bb4d6a647a459e30d23c3481aa1eafba7b61fff2
lbunix <loc@lbunix.com>
2026-02-03 17:50:45 -0800

    net-check.sh

---
+++ CIS217/net-check.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+ipaddr=$(ip a | grep inet | grep -v -e "127" -e "inet6")
+
+echo -e "\n### Initiating Net Check Script ###\n\n"
+sleep 2
+
+echo -e "This is your current IP: $ipaddr\n"
+echo -en "Let's check the DNS of a website. Please input the website URL: "
+read site
+
+echo -e "The DNS information of the site you had input is: "
+host -t ns "$site"
+sleep 1
+
+echo -e "\nLet's run a trace to the site you had chosen.\n"
+echo -e "Checking...\n\n"
+sleep 2
+
+traceroute "${site}"
+
+
+
+
