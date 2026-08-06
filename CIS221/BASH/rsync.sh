03e2308e307435b54679aaac75a52341d3981b07
lbunix <loc@lbunix.com>
2026-08-05 20:04:39 -0700

    Create rsync.sh

---
+++ CIS221/BASH/rsync.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# GLOBALS
+date="$(date +%m-%d-%Y_%H-%M-%S)"
+
+# GLOBALS
+
+while :
+do
+    echo "\n========== rsync =========="
+    echo -en "\nWhat is the remote username? "
+    read remoteUser
+
+    echo -en "\nWhat is the IP address of the remote server? "
+    read remoteAddress
+
+    if [[ -z "$remoteUser" || -z "$remoteAddress" ]]
+    then
+        echo "\nIncomplete entry. Please enter all fields."
+        continue
+    else
+        break
+    fi
+done
+
+backupDirectories=(
+    "/etc/"
+    "/home/"
+    "/root/"
+    "/var"
+)
+
+backup_folder="/home/$remoteUser/Documents/backups/$date"
+
+ssh "${remoteUser}@${remoteAddress}" "mkdir -p -- 'backup_folder'"
+
+if [[ $? -ne 0 ]]
+then
+    echo -e "\nRemote server connection failed."
+    exit 1
+fi
+
+echo -e "\nStarting backup.."
+
+sudo rsync -zavh -e ssh "${backupDirectories[@]}" "${remoteUser}@${remoteAddress}:${backup_folder}/"
+
+if [[ $? -eq 0 ]]
+then
+    echo -e "\nBackful successful!"
+else
+    echo -e "\nBackup failed."
+    exit 1
+fi
