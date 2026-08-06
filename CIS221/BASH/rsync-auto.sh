ef926518c93aa2e5ba7b7f1ab0a7eed7d4afe3cf
lbunix <loc@lbunix.com>
2026-08-05 20:52:41 -0700

    Create rsync-auto.sh

---
+++ CIS221/BASH/rsync-auto.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# GLOBALS
+date="$(date +%m-%d-%Y_%H-%M-%S)"
+
+remoteUser="loc"
+remoteAddress="192.168.101.165"
+sshKey="/home/loc/.ssh/rsync.key"
+
+# GLOBALS
+
+backupDirectories=(
+    "/etc"
+    "/home"
+    "/root"
+    "/var"
+)
+
+backup_folder="/home/$remoteUser/Documents/backups/$date"
+
+ssh -i "$sshKey" -o BatchMode=yes "${remoteUser}@${remoteAddress}" "mkdir -p -- 'backup_folder'"
+
+rsync -ae "ssh -i /$HOME/.ssh/rsync.key" "${backupDirectories[@]}" "${remoteUser}@${remoteAddress}:${backup_folder}/" > /dev/null
