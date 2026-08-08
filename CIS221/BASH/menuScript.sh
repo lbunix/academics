7694725f7680a5c1a81d761d5b3a4f30db1085e2
lbunix <loc@lbunix.com>
2026-08-07 18:44:39 -0700

    Update menuScript.sh

--- CIS221/BASH/menuScript.sh
+++ CIS221/BASH/menuScript.sh
@@ -1,8 +1,9 @@
+loc@lbunix1:~/Documents/LAB7$ cat menuScript.sh 
 #!/bin/bash
 
 # GLOBALS ################################
 
-quit = "no"
+quit="no"
 
 title() {
     echo ""
@@ -100,52 +101,30 @@ serverStatus() {
 octet='(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])'
 ip="^${octet}\.${octet}\.${octet}\.${octet}$"
 
-count=1
-targets=()
-
 # GLOBALS
 
-echo -e "\n======================"
-echo -e "Domain Upper Checker"
-echo -e "======================\n"
-
-echo -e "Enter 5 domains/ IP Addresses.\n"
-
-until [ "$count" -gt 5 ]
+while :
 do
-    echo -en "\nDomain# $count: "
-    read -r domains
+    echo -en "Enter the Domain/IP Address to check it's status > "
+    read address
 
-    target=""
-
-    if [[ "$domains" =~ $ip ]]
+    if [[ "$address" =~ $ip ]]
     then
-        target="$domains"
+        target="$address"
 
-    elif [[ "$domains" =~ [A-Za-z] ]]
+    elif [[ "$address" =~ [A-Za-z] ]]
     then
-        target=$(dig +short A "$domains" | grep -m 1 -E '^[0-9]+(\.[0-9]+){3}$')
-
+        target=$(dig +short A "$address" | grep -m 1 -E '^[0-9]+(\.[0-9]+){3}$')
+    else
+        echo "Incorrect input."
+        continue
     fi
 
-    targets+=("$target")
-    count=$((count + 1))
-done
-
-echo -e "\nAll five addresses entered. Starting pings...\n"
-
-for target in "${targets[@]}"
-do
     echo "Pinging $target..."
     echo "If ping does not reach, then you have input an incorrect domain/ IP address."
     ping -c 2 "$target" &
+
 done
-
-wait
-
-echo -e "\n======================"
-echo -e "Ping session complete."
-echo -e "======================\n"
 
 }
 
@@ -192,7 +171,7 @@ netToolsMenu=(
 netTools() {
 
 # GLOBALS
-quit = "no"
+quit="no"
 
 # GLOBALS
 
