14b50f5cfa133575126fbb12812f5caf11db2a38
lbunix <loc@lbunix.com>
2026-08-10 14:09:06 -0700

    Create foreignKeySet.sql

---
+++ CIS230/MS SQL/AgonTav/FK/foreignKeySet.sql
@@ -0,0 +1,37 @@
+USE CIS230_FINAL;
+GO
+
+-- FOREIGN KEY SET UP
+
+-- User Types
+INSERT INTO user_type ([name])
+VALUES
+	('admin'),
+	('player');
+
+-- Character Types
+-- 0 = NPC
+-- 1 = Playable
+INSERT INTO character_type ([type])
+VALUES
+	(0),
+	(1);
+
+-- Regions
+INSERT INTO region (region_name)
+VALUES
+	('NA'),
+	('EU'),
+	('AS'),
+	('OCE'),
+	('SA');
+
+-- Worlds
+INSERT INTO world (world_name, region_id)
+VALUES
+	('Wild Mount', 1),
+	('Luminara', 2),
+	('Grimhollow', 3),
+	('Morrowind', 4),
+	('Erebor', 5);
+GO
